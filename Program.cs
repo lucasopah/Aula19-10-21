2cae22537a79cfb4844e1ee11f9d487d89783b90
Lucas Gabiel <lucasbartholo@hotmail.com>
2021-10-20 18:58:10 -0300

    projeto incial

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double hora;
+            //double pagthora;
+
+
+            //Console.WriteLine("Informe horas trabalhadas");
+            //hora = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe o valor da hora");
+            //pagthora = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Seu salário é de"); 
+            //Console.WriteLine(hora * pagthora);
+
+
+            //exercicio 2
+
+
+            //int meses;
+            //int anos;
+            //int dias;
+
+            //Console.WriteLine("Informe os anos:");
+            // anos = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Informe os meses");
+            // meses = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Informe a quantidade de dias");
+            // dias = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Resulta em " + dias + "dias!");
+            // Console.WriteLine(dias = (meses * 30) + (anos * 365));
+
+
+            //exercicio 3
+
+            //double percentual;
+            //double salario_atual;
+
+
+            //Console.WriteLine("Informe o salario Atual");
+            //salario_atual = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe a % do aumento ");
+            //percentual = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Seu salário com aumento é de");
+            //Console.WriteLine((salario_atual * percentual / 100) + salario_atual);
+
+
+
+            //char funcionario;
+            //int vendas;
+            //double comissao = 30000*0.15;
+
+
+
+            //Console.WriteLine("Informe o nome do funcionário");
+            //funcionario = Console.ReadLine();
+            //Console.WriteLine("Informe a quantidade de vendas");
+            //vendas = int.Parse(Console.ReadLine());
+            //Console.WriteLine("A comissão é de" + funcionario + vendas*comissao);
+
+
+            //certo
+
+
+            //double salario;
+            //string nome;
+            //int qtdCarro;
+            //double carro = 30000 * 0.15;
+
+            //Console.WriteLine("Digite o nome do funcionário: ");
+            //nome = Console.ReadLine();
+
+            //Console.WriteLine("Digite o salário: ");
+            // salario = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Digite a quantidade de veículos: ");
+            // qtdCarro = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine("O salario será de R$: {0}", ((carro * qtdCarro) + salario));
+
+
+
+            /// EXERCICIO URNA
+            /// 
+            //int brancos, nulos, validos;
+            //int brancosP, nulosP, validosP;
+            //Console.WriteLine("Informe a quantidade de votos brancos");
+            //brancos = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe a quantidade de votos nulos");
+            //nulos = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe a quantidade de votos válidos");
+            //validos = int.Parse(Console.ReadLine());
+            //int total = brancos + nulos + validos;
+            //brancosP = (brancos * 100) / total;
+            //nulosP = (nulos * 100) / total;
+            //validosP = (validos * 100) / total;
+            //Console.WriteLine("O percentual de votos branco é de :" + brancosP);
+            //Console.WriteLine("O percentual de votos nulos é de :" + nulosP);
+            //Console.WriteLine("O percentual de votos validos é de :" + validosP);
+
+
+            // int numero;
+
+            //Console.WriteLine("Digite seu número");
+            //numero = int.Parse(Console.ReadLine());
+            //int antecessor = numero - 1;
+            //int sucessor = numero + 1;
+            //Console.WriteLine("seu antecessor é " + (antecessor) + "e o sucessor é" + (sucessor));
+
+
+            ////////////// exercicio
+            ///
+
+            //double nota1, nota2, nota3;
+
+            //Console.WriteLine("digite sua nota 1 ");
+            //nota1 = double.Parse(Console.Read());
+            //Console.WriteLine("digite sua nota 2 ");
+            //nota2 = double.Parse(Console.Read());
+            //Console.WriteLine("digite sua nota 3 ");
+            //nota3 = double.Parse(Console.Read());
+
+            
+            //Console.WriteLine("sua média foi de " + (nota1 + nota2 + nota3) / 3);
+
+        
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
